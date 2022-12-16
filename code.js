d1e08b5d3f567c5eb8b1c2c6255482d581eeb734
wakester <wakester2@gmail.com>
2022-12-16 10:45:34 -0500

    Make grid responsive to size user enters

--- code.js
+++ code.js
@@ -1,20 +1,36 @@
 
+function createGrid(size) {
+    let gridContainer = document.querySelector("#grid-container");
+
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    let totalBlocks = size * size;
+    let containerWidth = size * 25;
+    gridContainer.style.width = `${containerWidth}px`;
+
+    for(i=0; i < totalBlocks; i++) {
+        div = document.createElement("div");
+        gridContainer.appendChild(div).className = "grid-block";
+    }
+
+    let blockElements = document.querySelectorAll(".grid-block");
+    blockElements.forEach(block => {
+        block.addEventListener("mouseover", () => block.style.backgroundColor = "white");
+    });
+
+}
+
 function changeGridSize() {
-    console.log("The button has been clicked");
+    let size = Number(prompt("Enter size of gird (entering 16 will produce a 16x16 grid)"));
+    
+    if (Number.isInteger(size) && size < 100) {
+        createGrid(size);
+    } else {
+        window.alert("Please enter a valid number no greater than 100")
+    }
 }
 
-
-
-gridContainer = document.querySelector("#grid-container");
-
-for(i=0; i < 256; i++) {
-    div = document.createElement("div");
-    gridContainer.appendChild(div).className = "grid-block";
-}
-
-blockElements = document.querySelectorAll(".grid-block");
-blockElements.forEach(block => {
-    block.addEventListener("mouseover", () => block.style.backgroundColor = "white");
-});
-
+createGrid(16);
 document.getElementById("grid-size-btn").addEventListener("click", changeGridSize);
