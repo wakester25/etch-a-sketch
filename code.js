22fa473b5adeb3ef1e54b41d5b2d5eb59272845a
wakester <wakester2@gmail.com>
2022-12-30 10:33:30 -0500

    Add rainbow mode

--- code.js
+++ code.js
@@ -3,6 +3,28 @@ let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
+function createColorBlockEvent(color) {
+    let blockElements = document.querySelectorAll(".grid-block");
+    blockElements.forEach(block => {
+        block.addEventListener("mouseover", () => {
+            if(mouseDown) {block.style.backgroundColor = color}
+        });
+    });
+}
+
+function createRainbowBlockEvent() {
+    let rainbowValues = ['#9400D3', '#4B0082', '#0000FF', '#00FF00', '#FFFF00', '#FF7F00', '#FF0000'];
+    let blockElements = document.querySelectorAll(".grid-block");
+    blockElements.forEach(block => {
+        block.addEventListener("mouseover", () => {
+            if(mouseDown) {
+                let randomIndex = Math.floor(Math.random() * rainbowValues.length);
+                block.style.backgroundColor = rainbowValues[randomIndex];
+            }
+        });
+    });
+}
+
 function createGrid(size) {
     let gridContainer = document.querySelector("#grid-container");
 
@@ -19,15 +41,9 @@ function createGrid(size) {
         gridContainer.appendChild(div).className = "grid-block";
     }
 
-    let blockElements = document.querySelectorAll(".grid-block");
-    blockElements.forEach(block => {
-        block.addEventListener("mouseover", () => {
-            if(mouseDown) {block.style.backgroundColor = "white"}
-        });
-    });
+    createColorBlockEvent("white");
 
 }
-
 
 createGrid(16);
 slider = document.querySelector("#slider");
@@ -37,3 +53,5 @@ slider.addEventListener("change", () => {
     createGrid(slider.value);
     sliderCount.innerText = `${slider.value} X ${slider.value}`;
 });
+rainbowBtn = document.querySelector("#rainbow-btn");
+rainbowBtn.addEventListener("click", createRainbowBlockEvent);
